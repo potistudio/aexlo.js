69497a49297d8991202c15d051fa8496aec5aeab
POTI <potistudio2022@gmail.com>
2024-04-11 03:21:32 +0900

    add: Compiled dll for Debugging

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@
 const aexlo = require("..");
 
 const aexLoader = new aexlo.AEXLoader(); // {}
-console.log (aexlo.loadAEX("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\Debugger.aex")); // null
+console.log (aexlo.loadAEX("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\test\\Empty.aex")); // null
