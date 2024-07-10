f2bd7bf1244059dcb265c798693553097ab15958
POTI <potistudio2022@gmail.com>
2024-07-11 04:35:05 +0900

    ⚙ migrate from pnpm to yarn

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 
-const aexlo = require("..");
+import aexlo from "aexlo";
 
 const aexLoader = new aexlo.AEXLoader(); // {}
 console.log (aexlo.loadAEX("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\test\\Empty.aex")); // null
