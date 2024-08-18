7b0f40e7a5a09161ebc639664962fa7eb02d7af5
POTI <potistudio2022@gmail.com>
2024-08-19 00:47:36 +0900

    🎨 todo tree

--- test/test.js
+++ test/test.js
@@ -2,4 +2,10 @@
 import aexlo from "aexlo";
 
 const aexLoader = new aexlo.AEXLoader(); // {}
-console.log (aexlo.loadAEX("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\test\\SDK_Noise.aex")); // null
+//DONE: About
+//DONE: Global Setup
+//TODO: Params Setup
+//TODO: Render
+//TODO: Smart Pre Render
+//TODO: Smart Render
+aexlo.loadAEX("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\test\\SDK_Noise.aex"); // null
