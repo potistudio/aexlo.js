fd1b55a90c41ffd63a576af1e7a13f390f088ea8
POTI <potistudio2022@gmail.com>
2024-11-19 19:00:02 +0900

    ✨ separate reading & executing to each function

--- test/test.js
+++ test/test.js
@@ -1,26 +1,34 @@
 
 import aexlo from "aexlo";
 
-const instance = new aexlo.PluginInstance ("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\test\\QCA3.aex");
+const instance = new aexlo.PluginInstance ("D:\\Projects\\Development\\Node\\aexlo.js\\lib\\test\\OLMColorKey.aex");
 
-const resources = instance.loadResources();
-const kind = resources.split("MIB8dnik")[1].split("MIB8eman")[0];
-const name = resources.split("MIB8eman")[1].split("MIB8gtac")[0];
-const category = resources.split("MIB8gtac")[1].split("MIB84668")[0];
-const entry = resources.split("MIB84668")[1].split("MIB8RVPe")[0];
-const ripl = resources.split("MIB8RVPe")[1];
-const match = resources.split ("MIB8ANMe")[1].split ("MIB8LFea")[0];
-const url = resources.split ("MIB8LRUe")[1];
+loadResources();
+executeAll();
 
-console.log (`kind: ${kind}
-name: ${name}
-category: ${category}
-matchName: ${match}
-url: ${url}`);
-// console.log (instance.about());
-// console.log (instance.setupGlobal());
-// console.log (instance.setupParameters());
-// console.log (instance.render());
+function loadResources() {
+	const resources = instance.loadResources();
+	const kind = resources.split("MIB8dnik")[1].split("MIB8eman")[0];
+	const name = resources.split("MIB8eman")[1].split("MIB8gtac")[0];
+	const category = resources.split("MIB8gtac")[1].split("MIB84668")[0];
+	const entry = resources.split("MIB84668")[1].split("MIB8RVPe")[0];
+	const ripl = resources.split("MIB8RVPe")[1];
+	const match = resources.split ("MIB8ANMe")[1].split ("MIB8LFea")[0];
+	const url = resources.split ("MIB8LRUe")[1];
+
+	console.log (`kind: ${kind}
+	name: ${name}
+	category: ${category}
+	matchName: ${match}
+	url: ${url}`);
+}
+
+function executeAll() {
+	console.log (instance.about());
+	console.log (instance.setupGlobal());
+	console.log (instance.setupParameters());
+	console.log (instance.render());
+}
 
 //DONE: About
 //DONE: Global Setup
