651a9809fbf1dbe290090e58cfb0fa7ccecaedf9
POTI <potistudio2022@gmail.com>
2025-01-21 22:32:58 +0900

    👍 log name of enum

--- src/headers/param_data.hh
+++ src/headers/param_data.hh
@@ -4,6 +4,29 @@
 #include "./basic.hh"
 #include "./parameters/union.hh"
 
+enum class ParamType : int {
+	PF_Param_RESERVED = -1,
+	PF_Param_LAYER = 0,
+	PF_Param_SLIDER,
+	PF_Param_FIX_SLIDER,
+	PF_Param_ANGLE,
+	PF_Param_CHECKBOX,
+	PF_Param_COLOR,
+	PF_Param_POINT,
+	PF_Param_POPUP,
+	PF_Param_CUSTOM,
+	PF_Param_NO_DATA,
+	PF_Param_FLOAT_SLIDER,
+	PF_Param_ARBITRARY_DATA,
+	PF_Param_PATH,
+	PF_Param_GROUP_START,
+	PF_Param_GROUP_END,
+	PF_Param_BUTTON,
+	PF_Param_RESERVED2,
+	PF_Param_RESERVED3,
+	PF_Param_POINT_3D
+};
+
 typedef struct {
 	union {
 		int    id;
@@ -14,7 +37,7 @@ typedef struct {
 	short    ui_width;
 	short    ui_height;
 
-	int	    param_type;
+	ParamType	    param_type;
 	char    name[PF_MAX_EFFECT_PARAM_NAME_LEN + 1];
 	int	    flags;
 
